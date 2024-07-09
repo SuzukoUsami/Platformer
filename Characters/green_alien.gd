0c12f578deac43e368a31bd8da2752c79db4bb1f
SuzukoUsami <zuzanna.kamieniarz28@gmail.com>
2024-07-09 17:57:43 +0200

    walking animation added to player

--- Characters/green_alien.gd
+++ Characters/green_alien.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var walk_animation = $AnimatedSprite2D
+
 # Vertical impulse applied to the character upon bouncing over a mob in meters per second.
 @export var bounce_impulse = 16
 
@@ -7,6 +9,18 @@ var speed = 400.0
 var jump_speed = -550.0
 
 
+func _process(_delta):
+	if Input.is_action_just_pressed("right") or Input.is_action_just_pressed("left"):
+		walk_animation.play("walk")
+	elif Input.is_action_just_released("right") or Input.is_action_just_released("left"):
+		walk_animation.stop()
+		
+
+	if Input.is_action_just_pressed("right"):
+		walk_animation.set_flip_h(false)
+	elif Input.is_action_just_pressed("left"):
+		walk_animation.set_flip_h(true)
+	
 
 func hit():
 	Globals.health -= 1
@@ -17,7 +31,10 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func bounce(bounce_factor):
 	velocity.y = jump_speed * bounce_factor
-
+	
+#func bounce_away(bounce_factor):
+	#velocity.x = speed * bounce_factor
+	
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -34,5 +51,4 @@ func _physics_process(delta):
 	
 
 	move_and_slide()
-	
 
--- Scenes/Enemies/fly_enemy.gd
+++ Scenes/Enemies/fly_enemy.gd
@@ -8,8 +8,8 @@ func _process(_delta):
 	velocity = direction * speed
 	if active:
 		move_and_slide()
-		print(direction)
-		#look_at(Globals.player_position)
+		#look_at(Vector2(Globals.player_position.x,position.y))
+		#var kitku: Vector2 = Vector2(0.5,0.6)
 
 
 func _on_notice_area_body_entered(_body):
@@ -24,6 +24,7 @@ func _on_hit_area_body_entered(body):
 			body.bounce(1)
 		else:
 			Globals.hit_player(body)
+			#body.bounce_away(1)
 
 
 

60d4c85e1cb98024fee5bffc3f15b73a91345e22
SuzukoUsami <zuzanna.kamieniarz28@gmail.com>
2024-07-14 23:40:00 +0200

    added ButtonLevel3, continued working on level 2 design

--- Scenes/UI/main_menu.gd
+++ Scenes/UI/main_menu.gd
@@ -7,3 +7,7 @@ func _on_level_1_pressed():
 
 func _on_level_2_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Levels/level2.tscn")
+
+
+func _on_level_3_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/level3.tscn")

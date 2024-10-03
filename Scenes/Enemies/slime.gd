a6887f703ead28ed27fee7e5e5a4f32aeccd534d
SuzukoUsami <zuzanna.kamieniarz28@gmail.com>
2024-10-03 21:56:51 +0200

    slime has now red shader when dying

--- Scenes/Enemies/slime.gd
+++ Scenes/Enemies/slime.gd
@@ -9,17 +9,12 @@ func _physics_process(delta):
 	auto_move_component.handle_direction_switch()
 	position.x += speed * delta * auto_move_component.get_direction()
 
-func _on_hitbox_body_entered(body):
-	if (body.name == Globals.PLAYER_NAME):
-		var y_delta = position.y - body.position.y
-		if (y_delta > 40):
-			$AnimatedSprite2D.material.set_shader_parameter("progress", 0.6)
-			$HitShaderTimer.start()
-			body.bounce(1)
-		else:
-			body.hit_player()
 
+func _on_hitbox_component_going_to_die():
+	$AnimatedSprite2D.material.set_shader_parameter("progress", 0.6)
+	$HitShaderTimer.start()
 
 func _on_hit_shader_timer_timeout():
-	queue_free()
 	$AnimatedSprite2D.material.set_shader_parameter("progress", 0)
+	queue_free()
+	
